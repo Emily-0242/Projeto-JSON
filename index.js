ace9ff257cf6986c2153e1f5a0ea52e604e2a25c
Emily-0242 <evqv@discente.ifpe.edu.br>
2024-11-11 19:02:39 -0300

    projeto j-son

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+// Conexão com o banco de dados
+const mongoose = require("mongoose");
+mongoose.connect('mongodb+srv://evqv:SwJawRTAy62Z4A0H@cluster0.qw2ux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'); 
+
+const alunoRoutes = require("./routes/alunoRoutes");
+
+app.use(alunoRoutes);
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.listen("999", function() {
+    console.log("Rodando ...");
+});
