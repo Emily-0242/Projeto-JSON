dfc5be6f0fe160cf1daaba632c9333ece6c241f7
Emily-0242 <evqv#discente.ifpe.edu.br>
2024-11-18 16:27:49 -0300

    subindo

--- routes/alunoRoutes.js
+++ routes/alunoRoutes.js
@@ -3,10 +3,11 @@ const routes = express.Router();
 const alunoController = require("../controllers/alunoController");
 
 
-routes.get("/alunos/", alunoController.listar);
-routes.get("/alunos/cadastrar/", alunoController.cadastrarGet);
-routes.get("/alunos/:id", alunoController.filtrarAlunoId);
 routes.post("/alunos", alunoController.cadastrarPost);
+routes.get("/alunos/", alunoController.listar);
+routes.get("/alunos/cadastrar/:matricula?", alunoController.cadastrarGet);// pode vir parâmetro ou não.
+routes.get("/alunos/:id", alunoController.filtrarAlunoId);
+routes.get("/alunos/remover/:matricula", alunoController.remover);
 
 
 module.exports = routes;
