15724eba8cc32bb5c81248fb1093a455beb111df
Emily-0242 <evqv#discente.ifpe.edu.br>
2024-11-18 16:27:49 -0300

    subindo

--- controllers/alunoController.js
+++ controllers/alunoController.js
@@ -5,21 +5,36 @@ class AlunoController {
     static async listar(req, res) {
         const status = req.query.c;
         const alunos = await AlunoModel.find();
-        res.render("aluno/listagem", {alunos, status})
+        res.render("aluno/listagem", {alunos, status});
     }
 
     static async cadastrarPost(req, res) {
-        const novoAluno = new AlunoModel({
-            matricula: req.body.inp_matricula,
-            nome: req.body.inp_nome,
-            curso: req.body.inp_curso
-        });
-        await novoAluno.save();
-        res.redirect("/alunos?c=1");
+        if(req.body._id){//atualizar
+            await AlunoModel.findOneAndUpdate({_id: req.body._id},{
+                matricula:req.body.matricula,
+                nome: req.body.nome,
+                curso:req.body.curso
+            });
+            res.redirect("/alunos?c=3");
+        }else{//cadastrar
+            const novoAluno = new AlunoModel({
+                matricula: req.body.inp_matricula,
+                nome: req.body.inp_nome,
+                curso: req.body.inp_curso
+            });
+            await novoAluno.save();
+            res.redirect("/alunos?c=1");
+        }
+       
     }
 
     static async cadastrarGet(req, res) {
-        res.render("aluno/cadastrar");
+        const matricula = req.params.matricula;
+        let aluno = {};
+        if(matricula != undefined){
+            aluno = await AlunoModel.findOne({matricula});
+        }
+        res.render("aluno/cadastrar", {aluno});
     }
 
     static async filtrarAlunoId(req, res) {
@@ -27,6 +42,13 @@ class AlunoController {
         const aluno = await AlunoModel.findOne({matricula: id});
         res.render("aluno/detalhar", { aluno });
     }
+
+    static async remover(req, res) {
+        const mat = req.params.matricula;
+        await AlunoModel.deleteOne({ matricula: mat });
+        res.redirect("/alunos?c=2");
+    }
+
 }
 
 module.exports = AlunoController;
